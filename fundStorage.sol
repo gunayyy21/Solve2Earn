42e9a7ff611c244c9598361525516cb12739b4a3
Gunay <gunaykaraoglu21@gmail.com>
2022-07-10 20:25:23 +0300

    Initial commit

---
+++ fundStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0;
+
+// Contract for storing funds of users
+contract simpleStorage {
+    uint256 balance; // Balance for a specific user
+
+    // structure for storing users
+    struct User {
+        uint256 balance;
+        string name;
+    }
+
+    // Defining a variable of type User
+    User[] public users;
+}
