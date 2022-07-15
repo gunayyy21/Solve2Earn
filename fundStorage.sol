7903e96607b40e1b48351951e2632b2a1cbe3632
Günay <40771904+gunayyy21@users.noreply.github.com>
2022-07-15 12:45:20 +0300

    Add files via upload
    add_balance and add_user functions have been added
    get_user_balance mapping has been also added.

--- fundStorage.sol
+++ fundStorage.sol
@@ -8,10 +8,31 @@ contract simpleStorage {
 
     // structure for storing users
     struct User {
+        string user_name;
         uint256 balance;
-        string name;
+
     }
 
-    // Defining an array of type User
+    // Defining an array of type User to store users
     User[] public users;
+    
+    // mapping that returns user balances
+    mapping (string => uint256) public get_user_balance;
+
+    // This function adds rewarded tokens into the balances.
+    // Note: checking is done outside the contract, this function doesn't check
+    // if the person is eligible or not
+    function add_balance(uint256 new_tokens) public {
+        balance += new_tokens;
+    }
+
+    // adds a new user to the array with an initial balance
+    // initial balance = the award of the question if the user is a new user
+    function add_user(string memory _name, uint256 _initialBalance) public {
+        users.push(User(_name, _initialBalance));
+        get_user_balance[_name] = _initialBalance;
+    }
+
+
+
 }
